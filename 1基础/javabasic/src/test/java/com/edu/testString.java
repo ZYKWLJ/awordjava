30665ce89c66f707a90147d60f55f27ca3cc80c3
郑阳康 <2126483838@qq.com>
2025-02-23 19:57:52 +0800

    String的常用方法+StringBuilder+StringBuffer

--- 1基础/javabasic/src/test/java/com/edu/testString.java
+++ 1基础/javabasic/src/test/java/com/edu/testString.java
@@ -8,8 +8,9 @@ public class testString {
     @Test
     public void test1(){
         System.out.println("测试String类的常用方法");
-        String s1 = "helloAAAAAAAAAAA";
-        String lowerCase = s1.toLowerCase();
-        System.out.println(lowerCase);
+        String s1 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("1.基本方法\n");
+        System.out.println("length()="+s1.length()+"\nisEmpty()="+s1.isEmpty());
+        System.out.println("2.子串操作\n");
     }
 }
