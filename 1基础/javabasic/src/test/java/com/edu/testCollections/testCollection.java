6710f5b3107924972d1876ccdc6ac3f7e57d83bb
郑阳康 <2126483838@qq.com>
2025-02-27 21:37:00 +0800

    映射Map的分类及HashMap HashSet HashTable的区别

--- 1基础/javabasic/src/test/java/com/edu/testCollections/testCollection.java
+++ 1基础/javabasic/src/test/java/com/edu/testCollections/testCollection.java
@@ -1,8 +1,13 @@
 package com.edu.testCollections;
 
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class testCollection {
     Queue<Integer>queue=new LinkedList<>();
+    Map<Integer,Integer>map=new HashMap<>();
+    Map<Integer,Integer>map1=new Hashtable<>();
 }
