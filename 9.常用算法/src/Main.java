ca512f0b1266f8c7d685d37ef7eef3b1e15e3bf1
郑阳康 <2126483838@qq.com>
2025-02-28 18:51:28 +0800

    运行时数据区详解

--- 9.常用算法/src/Main.java
+++ 9.常用算法/src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        HeapSort heapSort = new HeapSort();
-        int arr[]={2,4,5,1,1,0,2,4,6};
-        heapSort.heapSort(arr, arr.length-1);
-        for (int i : arr) {
+        // HeapSort heapSort = new HeapSort();
+        // int arr[]={2,4,5,1,1,0,2,4,6};
+        // heapSort.heapSort(arr, arr.length-1);
+        // for (int i : arr) {
+        //     System.out.println(i+" ");
+        // }
+        // HeapSort heapSort = new HeapSort();
+        QuickSort quickSort = new QuickSort();
+        int arr1[]={2,4,5,1,1,0,2,4,6};
+        quickSort.quickSort(arr1,0, arr1.length-1);
+        for (int i : arr1) {
             System.out.println(i+" ");
         }
     }
