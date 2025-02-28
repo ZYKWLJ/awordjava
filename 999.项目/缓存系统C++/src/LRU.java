b6deee48dff3f18dbc985bb9889b6a4ceb64f63b
郑阳康 <2126483838@qq.com>
2025-02-28 10:54:45 +0800

    LRU算法新增容量限制+哈希同时删除别忘了！

--- 999.项目/缓存系统C++/src/LRU.java
+++ 999.项目/缓存系统C++/src/LRU.java
@@ -1,13 +1,16 @@
 import java.util.HashMap;
+import java.util.concurrent.CountDownLatch;
 
 class Node<E> {
     Node<E> ptrL, ptrR;
     E e;
+
     public Node(E e) {
         this.ptrL = null;
         this.ptrR = null;
         this.e = e;
     }
+
     public Node() {
     }
 }
@@ -17,9 +20,11 @@ public class LRU<E> {
     // 每一次删除L,每一次增加在R,LR为两个虚拟边界
     Node<E> L, R;
     HashMap<E, Node<E>> valAndNodeMap;// 记录值和节点的映射(因为我们是访问的值嘛~)
+    int capacity;// 新增缓存容量限制
+    int currentCapacity;
 
     // 初始化
-    public LRU() {
+    public LRU(int capacity) {
         // 初始化 L 和 R（记得要初始化哦~）
         L = new Node<E>(null);
         R = new Node<E>(null);
@@ -30,14 +35,22 @@ public class LRU<E> {
         L.ptrR = R;
         R.ptrL = L;
         R.ptrR = null;
+        this.capacity = capacity;// 新增缓存容量限制
+        this.currentCapacity = 0;
     }
 
     // 添加元素
     public E addE(E e) {
+        if (isOverCapacity()) {
+            System.out.println("Over Capacity, del the least use elem "+L.ptrR.e);
+            // 超容删除
+            delEWhileOverCapacity();
+        }
+        this.currentCapacity++;
         Node<E> newNode = new Node<>(e);
         // 判断是不是在链表中，在的话，先断开之前的元素
         if (valAndNodeMap.containsKey(e)) {
-            System.out.println("elem "+e+" had existed! break first!");
+            System.out.println("elem " + e + " had existed! break first!");
             Node<E> node = valAndNodeMap.get(e);
             node.ptrR.ptrL = node.ptrL;
             node.ptrL.ptrR = node.ptrR;
@@ -49,43 +62,64 @@ public class LRU<E> {
         newNode.ptrR = R;
         R.ptrL = newNode;
         valAndNodeMap.put(e, newNode);// 加入哈希表！
-        System.out.println("elem "+e+" add success!");
+        System.out.println("elem " + e + " add success!");
         return e;
     }
 
-// 删除元素
-     public void delE(E e){
-        if(!valAndNodeMap.containsKey(e)){
-            System.out.println("the aim elem "+e+" is not set in the table");
-            return ;
+    // 删除元素
+    public void delE(E e) {
+        if (!valAndNodeMap.containsKey(e)) {
+            System.out.println("the aim elem " + e + " is not set in the table");
+            return;
         }
         Node<E> node = valAndNodeMap.get(e);
-            node.ptrR.ptrL = node.ptrL;
-            node.ptrL.ptrR = node.ptrR;
-        System.out.println("del elem "+e+" success!");
-     }
+        node.ptrR.ptrL = node.ptrL;
+        node.ptrL.ptrR = node.ptrR;
+        this.currentCapacity--;
+        valAndNodeMap.remove(e);
+        System.out.println("del elem " + e + " success!");
+    }
 
-    //  打印元素
-    public void printE(){
+    // 打印元素
+    public void printE() {
         System.out.println("print the LRU:");
-        Node<E> node=L.ptrR;
-        while (node!=R) {
-            System.out.print(node.e+" ");
-            node=node.ptrR;
+        Node<E> node = L.ptrR;
+        while (node != R) {
+            System.out.print(node.e + " ");
+            node = node.ptrR;
         }
         System.out.println();
     }
 
+    // 判断是否超容
+    private boolean isOverCapacity() {
+        return this.capacity <= this.currentCapacity;
+    }
+
+    // 超容删除
+    private void delEWhileOverCapacity(){
+        valAndNodeMap.remove(L.ptrR.e);
+        L.ptrR=L.ptrR.ptrR;
+        L.ptrR.ptrL=L.ptrL;
+        
+    }
+
     public static void main(String[] args) {
-        LRU<String> lru = new LRU<String>();
-        for (int i = 0; i < 20000; i++) {
-            lru.addE(i+"");
+        LRU<String> lru = new LRU<String>(20);
+        for (int i = 0; i < 20; i++) {
+            lru.addE(i + "");
         }
         lru.printE();
 
-        for (int i = 0; i < 10; i++) {
-            lru.addE(i+"");
+        for (int i = 20; i < 40; i++) {
+            lru.addE(i + "");
         }
+        lru.printE();
+
+        for (int i = 2; i < 20; i++) {
+            lru.addE(i + "");
+        }
+        
         lru.printE();
     }
 }
