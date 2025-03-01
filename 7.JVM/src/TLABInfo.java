efed70e542220ff073a6de073d9c92e368b471e3
郑阳康 <2126483838@qq.com>
2025-03-01 10:44:25 +0800

    栈堆方法区的交互+JVM设计哲学

--- 7.JVM/src/TLABInfo.java
+++ 7.JVM/src/TLABInfo.java
@@ -1,7 +1,11 @@
 public class TLABInfo {
+    static int a=1;
     public static void main(String[] args) {
         // 简单的对象创建操作，触发 TLAB 分配
         Object obj = new Object();
         System.out.println("Object created: " + obj);
+        System.out.println(a);
+        a=2;
+        System.out.println(a);
     }
 }
