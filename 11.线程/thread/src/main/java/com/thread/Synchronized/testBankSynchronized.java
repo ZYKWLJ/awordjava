f0eb48b704941a3485e2b428660a62a87dfd3e49
郑阳康 <2126483838@qq.com>
2025-03-02 23:10:29 +0800

    synchronized的类锁仅仅只是Synchronized(XXX.Class)就行了！照样使用对象调用！

--- 11.线程/thread/src/main/java/com/thread/Synchronized/testBankSynchronized.java
+++ 11.线程/thread/src/main/java/com/thread/Synchronized/testBankSynchronized.java
@@ -1,88 +1,88 @@
-package com.thread.Synchronized;
+// package com.thread.Synchronized;
 
-class Bank {
-    int remainer;// 剩余的钱
-    String name;// 开户人
-    // private final ReentrantLock lock = new ReentrantLock();
-    // private Condition bankRemainerIsless = lock.newCondition();// 创建一个余额不足的条件变量
+// class Bank {
+//     int remainer;// 剩余的钱
+//     String name;// 开户人
+//     // private final ReentrantLock lock = new ReentrantLock();
+//     // private Condition bankRemainerIsless = lock.newCondition();// 创建一个余额不足的条件变量
 
-    public Bank(int remainer, String name) {
-        this.remainer = remainer;
-        this.name = name;
-    }
+//     public Bank(int remainer, String name) {
+//         this.remainer = remainer;
+//         this.name = name;
+//     }
 
-    public int getRemainer() {
-        return remainer;
-    }
+//     public int getRemainer() {
+//         return remainer;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    // 给别人转账
-    public static void transferToOther(Bank from ,Bank to, int amount) throws InterruptedException {
-        synchronized (Bank.class) {//这样用synchronized给整个类对象加锁！
-            while (from.remainer < amount) {
-                System.out.println(
-                        from.name + " wants to transfer " + amount + " to " + to.getName() + " ,but " + from.name
-                                + " 's remainer is " + from.remainer + " ,not enough, waiting other transfer to it...");
-                System.out.println(from.name + " is waiting...");
-                Bank.class.wait();
-                System.out.println(from.name + " is awakened...");
-            }
+//     // 给别人转账
+//     public static void transferToOther(Bank from ,Bank to, int amount) throws InterruptedException {
+//         synchronized (Bank.class) {//这样用synchronized给整个类对象加锁！
+//             while (from.remainer < amount) {
+//                 System.out.println(
+//                         from.name + " wants to transfer " + amount + " to " + to.getName() + " ,but " + from.name
+//                                 + " 's remainer is " + from.remainer + " ,not enough, waiting other transfer to it...");
+//                 System.out.println(from.name + " is waiting...");
+//                 Bank.class.wait();
+//                 System.out.println(from.name + " is awakened...");
+//             }
 
-            from.remainer -= amount;
-            to.remainer += amount;
-            System.out.println(from.name + " has transferred " + amount + "￥ to " + to.name + ", Now " + to.name
-                    + " 's amount is " + to.remainer);
-            Bank.class.notifyAll();
-            System.out.println(from.name + " called the notifyAll()....");
-        }
-    }
-}
+//             from.remainer -= amount;
+//             to.remainer += amount;
+//             System.out.println(from.name + " has transferred " + amount + "￥ to " + to.name + ", Now " + to.name
+//                     + " 's amount is " + to.remainer);
+//             Bank.class.notifyAll();
+//             System.out.println(from.name + " called the notifyAll()....");
+//         }
+//     }
+// }
 
-public class testBankSynchronized {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + " is running--Main\n");
+// public class testBankSynchronized {
+//     public static void main(String[] args) throws InterruptedException {
+//         System.out.println(Thread.currentThread().getName() + " is running--Main\n");
 
-        Bank A = new Bank(1000, "A");
-        Bank B = new Bank(2000, "B");
-        // 创建转账线程
-        Thread A2B = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " is running--A2B");
-            try {
-               Bank.transferToOther(A,B, 1500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+//         Bank A = new Bank(1000, "A");
+//         Bank B = new Bank(2000, "B");
+//         // 创建转账线程
+//         Thread A2B = new Thread(() -> {
+//             System.out.println(Thread.currentThread().getName() + " is running--A2B");
+//             try {
+//                Bank.transferToOther(A,B, 1500);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
+//         });
 
-        A2B.start();
-        Bank C = new Bank(1000, "C");
-        Thread C2A = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + " is running--C2A");
+//         A2B.start();
+//         Bank C = new Bank(1000, "C");
+//         Thread C2A = new Thread(() -> {
+//             System.out.println(Thread.currentThread().getName() + " is running--C2A");
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//             try {
+//                 Thread.sleep(1000);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
 
-            try {
-                Bank.transferToOther(C,A, 600);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//             try {
+//                 Bank.transferToOther(C,A, 600);
+//             } catch (InterruptedException e) {
+//                 e.printStackTrace();
+//             }
 
-            System.out.println();
-        });
-        C2A.start();
+//             System.out.println();
+//         });
+//         C2A.start();
 
-        // 确保主线程是必须等到A2B完成时才退出！
-        try {
-            A2B.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//         // 确保主线程是必须等到A2B完成时才退出！
+//         try {
+//             A2B.join();
+//         } catch (InterruptedException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
