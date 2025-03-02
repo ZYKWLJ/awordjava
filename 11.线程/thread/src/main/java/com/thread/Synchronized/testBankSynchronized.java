5a4058895a25b098d4798c7aa7578a8ff934089d
郑阳康 <2126483838@qq.com>
2025-03-02 18:47:12 +0800

    同样适用synchronized来模拟转账还是会出现死锁，问题究竟是怎么回事？

--- 11.线程/thread/src/main/java/com/thread/Synchronized/testBankSynchronized.java
+++ 11.线程/thread/src/main/java/com/thread/Synchronized/testBankSynchronized.java
@@ -1,4 +1,4 @@
-package com.thread.Lock;
+package com.thread.Synchronized;
 
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -6,8 +6,8 @@ import java.util.concurrent.locks.ReentrantLock;
 class Bank {
     int remainer;// 剩余的钱
     String name;// 开户人
-    private final ReentrantLock lock = new ReentrantLock();
-    private Condition bankRemainerIsless = lock.newCondition();// 创建一个余额不足的条件变量
+    // private final ReentrantLock lock = new ReentrantLock();
+    // private Condition bankRemainerIsless = lock.newCondition();// 创建一个余额不足的条件变量
 
     public Bank(int remainer, String name) {
         this.remainer = remainer;
@@ -23,15 +23,16 @@ class Bank {
     }
 
     // 给别人转账
-    public void transferToOther(Bank b, int amount) throws InterruptedException {
-        lock.lock();
+    public synchronized void  transferToOther(Bank b, int amount) throws InterruptedException {
+        // lock.lock();
         try {
             while (this.remainer < amount) {// 余额不足，A暂停转账，等别人转给他,一直查询，防止虚假唤醒！
                 System.out.println(
                         this.name + " wants to transfer " + amount + " to " + b.getName() + " ,but " + this.name
                                 + " 's remainer is " + this.remainer + " ,not enough, waiting other transfer to it...");
                 System.out.println(this.name + " is waiting...");
-                bankRemainerIsless.await();// ...暂停。。。。等待别人给他转账了才能继续！
+                // bankRemainerIsless.await();// ...暂停。。。。等待别人给他转账了才能继续！
+                this.wait();
                 System.out.println(this.name + " is awakened...");
             }
 
@@ -39,27 +40,28 @@ class Bank {
             b.remainer += amount;// 转账顺利，B增加钱
             System.out.println(this.name + " has transfers " + amount + "￥ to " + b.name + ", Now " + b.name
                     + " 's amount is " + b.remainer);
-            bankRemainerIsless.signalAll();
-            System.out.println(this.name+" called the signaiAll()....");
-
+            this.notifyAll();
+            // bankRemainerIsless.signalAll();
+            // System.out.println(this.name + " called the signaiAll()....");
+            System.out.println(this.name + " called the notifyAll()....");
         } catch (InterruptedException e) {// 对 await() 方法抛出的 InterruptedException 进行单独处理，并恢复中断状态。
             System.out.println(this.name + " is interrupted during waiting.");
             Thread.currentThread().interrupt();
         } finally {
-            lock.unlock();
+            // lock.unlock();
         }
     }
 }
 
-public class testBankCondition3 {
+public class testBankSynchronized {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName()+" is running--Main\n");
+        System.out.println(Thread.currentThread().getName() + " is running--Main\n");
 
         Bank A = new Bank(1000, "A");
         Bank B = new Bank(2000, "B");
         // 创建转账线程
         Thread A2B = new Thread(() -> {
-        System.out.println(Thread.currentThread().getName()+" is running--A2B");
+            System.out.println(Thread.currentThread().getName() + " is running--A2B");
             try {
                 A.transferToOther(B, 1500);
             } catch (InterruptedException e) {
@@ -70,7 +72,7 @@ public class testBankCondition3 {
         A2B.start();
         Bank C = new Bank(1000, "C");
         Thread C2A = new Thread(() -> {
-        System.out.println(Thread.currentThread().getName()+" is running--C2A");
+            System.out.println(Thread.currentThread().getName() + " is running--C2A");
 
             try {
                 Thread.sleep(1000);
