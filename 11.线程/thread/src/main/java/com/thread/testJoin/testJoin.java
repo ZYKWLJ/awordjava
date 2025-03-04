c21a69d67fe4a2a105f1009237ee0636c96ea071
郑阳康 <2126483838@qq.com>
2025-03-04 16:43:23 +0800

    实现特定线程执行顺序1—join方法

---
+++ 11.线程/thread/src/main/java/com/thread/testJoin/testJoin.java
@@ -0,0 +1,23 @@
+package com.thread.testJoin;
+
+public class testJoin {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            System.out.println("my first execute~");
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("my second execute~~");
+        });
+        
+        t1.start();
+        t2.start();
+        t2.join();
+        System.out.println("my third execute~~~");
+    }
+}
