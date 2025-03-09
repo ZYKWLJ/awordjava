f103be3e979d4c4429a14e61782163025a1dc57c
郑阳康 <2126483838@qq.com>
2025-03-09 17:29:35 +0800

    纯与不纯的责任链模式对比，本质在于是不是一定处理请求

--- 14.设计模式/3.11个行为型(交互艺术)模式/1.责任链模式/src/v2/Client.java
+++ 14.设计模式/3.11个行为型(交互艺术)模式/1.责任链模式/src/v2/Client.java
@@ -2,7 +2,7 @@ package v2;
 public class Client {
     public static void main(String[] args) {
         PurchaseRequest purchaseRequest1 = new PurchaseRequest(40000,"给公司买10台笔记本");
-        PurchaseRequest purchaseRequest2 = new PurchaseRequest(5000000,"给公司购置地皮建新楼");
+        PurchaseRequest purchaseRequest2 = new PurchaseRequest(50000000,"给公司购置地皮建新楼");
         PurchaseRequest purchaseRequest3 = new PurchaseRequest(70000,"公司培训新职员教培支出");
         // 审批人
         Handler director=new ConcreteHandlerDirector("主任部张三");
